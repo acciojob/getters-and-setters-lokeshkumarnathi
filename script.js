060026dbd24314418a38f4a4d1938483c4fff379
LOKESH KUMAR NATHI <147913462+lokeshkumarnathi@users.noreply.github.com>
2026-09-22 11:00:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-//complete this code
-class Person {}
 
-class Student extends Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Teacher extends Person {}
+  get name() {
+    return this._name;
+  }
 
-// Do not change the code below this line
+  set name(name) {
+    this._name = name;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(age) {
+    this._age = age;
+  }
+}
+
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
+
+// Make classes available to Cypress
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
+
